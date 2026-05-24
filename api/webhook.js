229322c8f54e068c7d15511c88a7c600372b0af9
masari <cl4xyre@gmail.com>
2026-05-24 21:36:54 +0300

    webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
 
+    const webhook = process.env.DISCORD_WEBHOOK;
+
+    await fetch(webhook, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req.body)
+    });
+
+    res.status(200).json({
+        success: true
+    });
+}
